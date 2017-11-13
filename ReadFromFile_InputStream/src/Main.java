7d1cbb2bc3337ad2feb4b6472aaf7f8b45286c21
Iancu Iulian <iulian.iancu1995@gmail.com>
2017-11-14 01:50:29 +0200

    Update Main.java

--- ReadFromFile_InputStream/src/Main.java
+++ ReadFromFile_InputStream/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         ReadFromFile_InputStream rff = new ReadFromFile_InputStream("data/myData.txt");
         String s = rff.getDataString();
 
