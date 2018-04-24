a1386a7b40b50fb28eee7795203b0bad34545973
Iulian Iancu <iulian.iancu1995@gmail.com>
2018-04-24 10:21:46 +0300

    Update Singleton.java

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -2,20 +2,14 @@
  * Created by ulise on 11/04/2017.
  */
 public class Singleton {
-
     private static Singleton firstInstance = null;
 
     private Singleton () {}
 
     public static Singleton getInstance(){
-
         if (firstInstance == null){
             firstInstance = new Singleton();
         }
-
         return firstInstance;
     }
-
-
-
 }
